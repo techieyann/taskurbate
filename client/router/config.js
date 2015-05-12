81f737ae0a08f2b09a0cbabe05461f6acd8261a0
Ian McEachern <techieyann@gmail.com>
2015-05-12 16:15:31 -0700

    router progress bar init, waves effect on mobile buttons

--- client/router/config.js
+++ client/router/config.js
@@ -2,6 +2,8 @@ Router.configure({
 	layoutTemplate: 'layout',
 	notFoundTemplate: 'error404',
 	loadingTemplate: 'loading',
+	progressDelay: 100,
+	progressSpinner: false
 });
 
 Router.onBeforeAction('loading');
